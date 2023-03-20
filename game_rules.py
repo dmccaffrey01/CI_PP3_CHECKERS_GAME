c9259003df818972095c895df093bc133452e303
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-03-20 17:54:53 +0000

    Create separate file for game rules

---
+++ game_rules.py
@@ -0,0 +1,106 @@
+import colorama
+from colorama import Fore, Back, Style
+import os
+import time
+import sys
+import display
+import main_menu as mm
+
+#Initialize colorama
+colorama.init(autoreset=True)
+
+def display_game_rules():
+    """
+    Displays the game rules 
+    """
+    display.cls()
+    for i in range(3):
+        print(" ")
+
+    print(f"{game_rules_heading() + game_rules_top_and_bottom_line() + game_rules_empty_line() + format_game_rules_lines() + game_rules_empty_line() + game_rules_top_and_bottom_line()}")
+
+    if ask_user_to_exit_game_rules():
+        display.welcome()
+        print(Fore.YELLOW + "Returning to main menu...")
+        time.sleep(1)
+        mm.main_menu_screen()
+        
+def ask_user_to_exit_game_rules():
+    """
+    Asks the user to return to the main menu
+    """
+    print(Fore.YELLOW + "Press 1 or r to return to the main menu:")
+    options = "1) Return to the main menu\n"
+    option_selected = input(options)
+    while True:
+        validated_option = validate_exit_game_rules_input(option_selected)
+        if validated_option:
+            return validated_option
+            break
+        display.new_line()
+        print(Fore.YELLOW + "Please input 1 or r:")
+        option_selected = input(options)    
+
+def validate_exit_game_rules_input(option):
+    """
+    Validates if input is 1 or r
+    """
+    if option == "1" or option.lower() == "one" or option == "r":
+        return "return"
+    else:
+        return False
+
+def game_rules_heading():
+    """
+    Returns f string of game rules heading 
+    """
+    return f"{' ' * 46 + Fore.CYAN + 'G A M E   R U L E S'}\n"
+
+def get_game_rules_lines():
+    """ 
+    Returns the lines of strings in a list
+    """
+    file = open("game_rules.txt", "r")
+    lines = file.readlines()
+    formatted_lines = []
+    for line in lines:
+        formatted_lines.append(line.strip())
+    return formatted_lines
+
+def format_game_rules_lines():
+    """
+    Returns f string of lines put together 
+    """
+    formatted_line = f""
+    lines = get_game_rules_lines()
+    for line in lines:
+        formatted_line += format_game_rule_line(line)
+    return formatted_line
+
+def format_game_rule_line(line):
+    """ 
+    Return f string of line
+    """
+    if line == "nl":
+        return game_rules_empty_line()
+    else:
+        return f"{game_rules_start_of_line() + Fore.WHITE + line + ' ' * (100 - len(line)) + Fore.YELLOW + '|'}\n"
+
+def game_rules_top_and_bottom_line():
+    """
+    Returns f string 
+    """
+    return f"{' ' * 6 + Fore.YELLOW + '=' * 106}\n"
+   
+
+def game_rules_empty_line():
+    """
+    Returns f string 
+    """
+    return f"{' ' * 6 + Fore.YELLOW + '|' + ' ' * 104 + Fore.YELLOW + '|'}\n"
+
+def game_rules_start_of_line():
+    """
+    Returns f string 
+    """
+    return f"{' ' * 6 + Fore.YELLOW + '|' + ' ' * 4}"
--- main_menu.py
+++ main_menu.py
@@ -11,6 +11,7 @@ from operator import itemgetter
 import sys
 import leaderboard
 import display
+import game_rules as gr
 
 #Initialize colorama
 colorama.init(autoreset=True)
@@ -72,7 +73,7 @@ def main_menu_selection(option):
     if option == 1:
         get_num_players()
     elif option == 2:
-        display_game_rules()
+        gr.display_game_rules()
     elif option == 3:
         leaderboard.go_to_leaderboard()
     elif option == 4:
@@ -547,101 +548,6 @@ def start_checkers_game(player1, player2, num):
     """
     checkers.start_game(player1, player2, num)
     return True
-
-def display_game_rules():
-    """
-    Displays the game rules 
-    """
-    display.cls()
-    for i in range(3):
-        print(" ")
-
-    print(f"{game_rules_heading() + game_rules_top_and_bottom_line() + game_rules_empty_line() + format_game_rules_lines() + game_rules_empty_line() + game_rules_top_and_bottom_line()}")
-
-    if ask_user_to_exit_game_rules():
-        display.welcome()
-        print(Fore.YELLOW + "Returning to main menu...")
-        time.sleep(1)
-        main_menu_screen()
-        
-def ask_user_to_exit_game_rules():
-    """
-    Asks the user to return to the main menu
-    """
-    print(Fore.YELLOW + "Press 1 or r to return to the main menu:")
-    options = "1) Return to the main menu\n"
-    option_selected = input(options)
-    while True:
-        if validate_exit_game_rules_input(option_selected):
-            return validate_exit_game_rules_input(option_selected)
-            break
-        display.new_line()
-        print(Fore.YELLOW + "Please input 1 or r:")
-        option_selected = input(options)    
-
-def validate_exit_game_rules_input(option):
-    """
-    Validates if input is 1 or r
-    """
-    if option == "1" or option.lower() == "one" or option == "r":
-        return "return"
-    else:
-        return False
-
-def game_rules_heading():
-    """
-    Returns f string of game rules heading 
-    """
-    return f"{' ' * 46 + Fore.CYAN + 'G A M E   R U L E S'}\n"
-
-def get_game_rules_lines():
-    """ 
-    Returns the lines of strings in a list
-    """
-    file = open("game_rules.txt", "r")
-    lines = file.readlines()
-    formatted_lines = []
-    for line in lines:
-        formatted_lines.append(line.strip())
-    return formatted_lines
-
-def format_game_rules_lines():
-    """
-    Returns f string of lines put together 
-    """
-    formatted_line = f""
-    lines = get_game_rules_lines()
-    for line in lines:
-        formatted_line += format_game_rule_line(line)
-    return formatted_line
-
-def format_game_rule_line(line):
-    """ 
-    Return f string of line
-    """
-    if line == "nl":
-        return game_rules_empty_line()
-    else:
-        return f"{game_rules_start_of_line() + Fore.WHITE + line + ' ' * (100 - len(line)) + Fore.YELLOW + '|'}\n"
-
-def game_rules_top_and_bottom_line():
-    """
-    Returns f string 
-    """
-    return f"{' ' * 6 + Fore.YELLOW + '=' * 106}\n"
-   
-
-def game_rules_empty_line():
-    """
-    Returns f string 
-    """
-    return f"{' ' * 6 + Fore.YELLOW + '|' + ' ' * 104 + Fore.YELLOW + '|'}\n"
-
-def game_rules_start_of_line():
-    """
-    Returns f string 
-    """
-    return f"{' ' * 6 + Fore.YELLOW + '|' + ' ' * 4}"
 
 def exit_game():
     """ 
