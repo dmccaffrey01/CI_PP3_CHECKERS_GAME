1e631e4e7bd56aed5d612d7ead1b01fc35017456
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-03-23 22:16:57 +0000

    Add unit test for validate ask feature testing

--- test/test_feature_testing.py
+++ test/test_feature_testing.py
@@ -44,6 +44,16 @@ class TestFeatureTesting(unittest.TestCase):
     def test_go_to_feature_testing_return(self):
         self.assertEqual(ft.go_to_feature_testing(), "return_to_main_menu")
 
+    def test_validate_ask_feature_testing(self):
+        self.assertEqual(ft.validate_ask_feature_testing("1"), 1)
+        self.assertEqual(ft.validate_ask_feature_testing("2"), 2)
+        self.assertEqual(ft.validate_ask_feature_testing("3"), 3)
+        self.assertEqual(ft.validate_ask_feature_testing("4"), 4)
+        self.assertEqual(ft.validate_ask_feature_testing("5"), 5)
+        self.assertEqual(ft.validate_ask_feature_testing("6"), 6)
+        self.assertEqual(ft.validate_ask_feature_testing("r"), "return")
+        self.assertEqual(ft.validate_ask_feature_testing("wrong"), False)
+
 
 if __name__ == "__main__":
     unittest.main()
