a1ccf83758cb7ec60ca55aee40f89ebcb18daa65
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-03-27 22:00:15 +0000

    Change moves for draw and message

--- checkers_folder/checkers.py
+++ checkers_folder/checkers.py
@@ -71,7 +71,7 @@ def start_game_loop(game_state, p1, p2, player1, player2, num, test,
             or (color == "white" and not p2)
 
         movable_pieces = game_state.get_movable_pieces(color)
-        if not movable_pieces or moves >= 100:
+        if not movable_pieces or moves >= 150:
             game_over = True
             display_game_over(game_state, moves, p1, p2, player1, player2,
                               num, test, original_board)
@@ -448,8 +448,8 @@ def check_winner(game_state, moves, type, p1, p2, player1, player2):
     """
     Checks who the winner is and formats a string to return
     """
-    if moves >= 100:
-        message = "Stalemate: The game is a draw"
+    if moves >= 150:
+        message = "DRAW"
     elif smf.score_the_pieces_on_board(game_state.board) > 0:
         if type == "color":
             message = "Black"
